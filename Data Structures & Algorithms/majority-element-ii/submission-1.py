01b836331c4f392bcf8fa604f8b3f5f589b3e82c
Isaac Rojas Sosa <156484215+IsaacRoSosa@users.noreply.github.com>
2026-05-12 11:53:23 -0600

    Add: majority-element-ii - submission-1

---
+++ Data Structures & Algorithms/majority-element-ii/submission-1.py
@@ -0,0 +1,43 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        ans = []
+        target = len(nums)/3
+        """
+        #Boyer-Moore Voting ALgorithm
+        """
+        cand1 , cand2 = None, None
+        count1, count2 = 0, 0
+
+        for num in nums:
+            if num == cand1:
+                count1 += 1
+            elif num == cand2:
+                count2 += 1
+            elif count1 == 0:
+                cand1 = num
+                count1 = 1
+            elif count2 == 0:
+                cand2 = num
+                count2 = 1
+            else:
+                count1 -=1
+                count2 -=1
+
+        count1, count2 = 0, 0
+        for num in nums:
+            if cand1 is not None and num == cand1:
+                count1 +=1
+                if count1 > target and num not in ans:
+                    ans.append(num)
+            elif cand2 is not None and num == cand2:
+                count2 +=1
+                if count2 > target and num not in ans:
+                    ans.append(num)
+        return ans
+
+
+
+
+        
+            
+
